73749f4610a46601dcc27ca356c67145df44f1cb
Luqman PC <luqmanarshad05@gmail.com>
2025-11-26 01:10:45 +0500

    Added Time Portal Mechanic with backgrounds and transitions

---
+++ ChronoQuest/Assets/Scripts/Transitions/FinishPoint.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FinishPoint : MonoBehaviour
+{
+    [SerializeField] string levelName;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneController.instance.LoadScene(levelName);
+        }
+    }
+}
